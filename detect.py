c004fb4316477a12aff55f68e618ad88620dc3cf
Tom Auger <tomauger@live.com>
2021-03-29 23:43:59 +0100

    debug change

--- detect.py
+++ detect.py
@@ -111,10 +111,12 @@ def detect(save_img=False):
                     for *xyxy, conf, cls in det:
                         if save_txt:  # Write to file
                             # xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                            coords = (torch.tensor(xyxy).view(1, 4) / gn).view(-1).tolist()
+                            coords = torch.tensor(xyxy).view(1, 4).view(-1).tolist()
                             # with open(txt_path + '.txt', 'a') as f:
                             #     f.write(('%g ' * 5 + '\n') % (cls, *xywh))  # label format
-                            f.write(('%g ' * 6) % (cls, conf, *coords))  # label format
+                            towrite = ('%g ' * 6) % (cls, conf, *coords)
+                            f.write(towrite)  # label format
+                            print(towrite)
 
                         if save_img or view_img:  # Add bbox to image
                             label = '%s' % (names[int(cls)])
