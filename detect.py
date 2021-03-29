ca2aa23371290edecf52a1b5fcd3199cc54e24f2
Tom Auger <tomauger@live.com>
2021-03-29 23:42:24 +0100

    fix format issue

--- detect.py
+++ detect.py
@@ -111,12 +111,9 @@ def detect(save_img=False):
                     for *xyxy, conf, cls in det:
                         if save_txt:  # Write to file
                             # xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                            coords = (torch.tensor(xyxy).view(1, 4) / gn).tolist()
+                            coords = (torch.tensor(xyxy).view(1, 4) / gn).view(-1).tolist()
                             # with open(txt_path + '.txt', 'a') as f:
                             #     f.write(('%g ' * 5 + '\n') % (cls, *xywh))  # label format
-                            print(cls)
-                            print(conf)
-                            print(*coords)
                             f.write(('%g ' * 6) % (cls, conf, *coords))  # label format
 
                         if save_img or view_img:  # Add bbox to image
